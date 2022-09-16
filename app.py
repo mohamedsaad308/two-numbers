11353de63bb785e08fad7abbc816ca9e8fc9b07d
mohamedsaad308 <mohamedsaad308@gmail.com>
2022-09-17 00:32:30 +0200

    WIP: working with corner cases

--- app.py
+++ app.py
@@ -1,6 +1,55 @@
-from flask import Flask
-app = Flask(__name__)
+from unittest import result
+from flask import Flask, request, abort
 
-@app.route('/api/two-numbers', methods=['POST'])
-def hello_world():
-    return 'Hello, World!'
+
+def create_app(test_config=None):
+
+    app = Flask(__name__)
+
+    @app.route('/api/two-numbers', methods=['POST'])
+    def hello_world():
+        """
+        This endpoint should take two numbers num1 and num2 between ±100*10^9
+        and return 4 arithmetic operations between them
+        Addition: num1 + num2
+        Subtraction: num1 - num2
+        Multiplication: nun1 x num2
+        Division: num1 / num2
+        """
+        body = request.get_json()
+        if body is None:
+            abort(400)
+
+        num1 = body.get("num1", None)
+        num2 = body.get("num2", None)
+        if num1 is None or num2 is None:
+            abort(400)
+        try:
+            num1 = float(num1)
+            num2 = float(num2)
+
+        except ValueError:
+            abort(400)
+        if min(num1, num2) < -100 * 10**9 or max(num1, num2) > 100 * 10**9:
+            abort(400)
+        
+        result = {}
+        result["addition"] = num1 + num2
+        result["substraction"] = num1 - num2 
+        result["multiplication"] = num1*num2
+        if num2 == 0:
+            result["division"] = "Error, Zero division"
+        else:
+            result["division"] = num1/num2
+        return {
+                'success': True,
+                'results' : result
+            }
+    @app.errorhandler(400)
+    def bad_request(error):
+        return {
+            "success": False,
+            "error": 400,
+            "message": "Please provide two numbers between plus and minus one hundred billions"
+        }, 400
+    return app
